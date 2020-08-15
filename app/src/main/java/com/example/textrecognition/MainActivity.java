0400666e366e344f247ae9e2ad3e01b20f1ba517
NuwanKeshara <nuwankyazh@gmail.com>
2020-08-15 23:11:26 +0530

    first commit

---
+++ app/src/main/java/com/example/textrecognition/MainActivity.java
@@ -0,0 +1,140 @@
+package com.example.textrecognition;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button snapBtn;
+    private Button detectBtn;
+    Button imageBtn;
+    private ImageView imageView;
+    private TextView txtView;
+    private Bitmap imageBitmap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        snapBtn = findViewById(R.id.snapBtn);
+        detectBtn = findViewById(R.id.detectBtn);
+        imageBtn = findViewById(R.id.setImageBtn);
+        imageView = findViewById(R.id.imageView);
+        txtView = findViewById(R.id.txtView);
+        snapBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dispatchTakePictureIntent();
+            }
+        });
+        detectBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                detectTxt();
+            }
+        });
+        imageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectImage();
+            }
+        });
+    }
+
+    static final int REQUEST_IMAGE_SELECT = 2;
+
+    private void selectImage(){
+        Intent selectImage = new Intent(Intent.ACTION_GET_CONTENT);
+        selectImage.setType("image/*");
+        startActivityForResult(Intent.createChooser(selectImage,"pick an image"),REQUEST_IMAGE_SELECT);
+    }
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+        }
+        else if(requestCode == REQUEST_IMAGE_SELECT && resultCode == RESULT_OK){
+            try {
+                InputStream inputStream = getContentResolver().openInputStream(data.getData());
+                imageBitmap = BitmapFactory.decodeStream(inputStream);
+                imageView.setImageBitmap(imageBitmap);
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void detectTxt() {
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(imageBitmap);
+        FirebaseVisionTextRecognizer textRecognizer  = FirebaseVision.getInstance().getOnDeviceTextRecognizer();
+
+        Task<FirebaseVisionText> task = textRecognizer.processImage(image);
+        task.addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
+            @Override
+            public void onSuccess(FirebaseVisionText firebaseVisionText) {
+                processTxt(firebaseVisionText);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+            }
+        });
+    }
+
+    private void processTxt(FirebaseVisionText text) {
+        List<FirebaseVisionText.TextBlock> blocks = text.getTextBlocks();
+        if (blocks.size() == 0) {
+            Toast.makeText(MainActivity.this, "No Text :(", Toast.LENGTH_LONG).show();
+            return;
+        }
+      //  for (FirebaseVisionText.TextBlock block : blocks) {
+
+
+                    String txt = text.getText();
+                    txtView.setTextSize(16);
+                    txtView.setText(txt);
+
+
+       // }
+    }
+
+}
